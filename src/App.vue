8f451491a2bfd77965ddbb04dd8ae17dda8ecf5a
Laura <lauraporto596@gmail.com>
2021-07-13 09:59:16 +0200

    mapState movies example

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <div id="app">
     <p>Tu nombre es : {{ nombre }}</p>
     <p>Tu edad es : {{ edad }}</p>
-    <Contador> Ejemplo de Vuex </Contador>
+    <Contador></Contador>
+    <h1>Estas son tus películas :</h1>
+    <Peliculas></Peliculas>
   </div>
 </template>
 
 <script>
 import Contador from "./components/Contador";
+import Peliculas from "./components/Peliculas";
 import { mapState } from "vuex";
 export default {
   name: "App",
   components: {
     Contador,
+    Peliculas,
   },
   computed: mapState({
     nombre: (state) => state.nombre,
---
+++ src/components/Peliculas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <li v-bind:key="pelicula.titulo" v-for="pelicula in peliculas">
+      {{ pelicula.titulo }}
+    </li>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "Peliculas",
+  computed: mapState(["peliculas"]),
+};
+</script>
+
+<style lang="scss" scoped></style>
