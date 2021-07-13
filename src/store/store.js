5f5577e11f35a04fec108835e6cfa2c82c2dfac2
Laura <lauraporto596@gmail.com>
2021-07-13 09:42:13 +0200

    new object in store, mapgetters

--- src/store/store.js
+++ src/store/store.js
@@ -8,6 +8,12 @@ export const store = new Vuex.Store({
     contador: 0,
     nombre: "Laura",
     edad: 27,
+    peliculas: [
+      { titulo: "Pulp Fiction", año: 1994, active: false },
+      { titulo: "La quimera del oro", año: 1925, active: false },
+      { titulo: "Miedo y asco en Las Vegas", año: 1998, active: false },
+      { titulo: "Blade Runner", año: 1982, active: false },
+    ],
   },
   mutations: {
     sumar(state) {
